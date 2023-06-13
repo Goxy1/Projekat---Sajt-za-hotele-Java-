57a849e771e5ef9c9f26cadb989eae3772769437
Goxy1 <uros.gojak001@gmail.com>
2023-06-14 01:30:40 +0200

    Created AtlantisAddTypeOfRoomFirstSlotServlet
    Servlet that changes the type of room in the database.

---
+++ src/main/java/Servlets/AtlantisAddTypeOfRoomFirstSlotServlet.java
@@ -0,0 +1,43 @@
+package Servlets;
+
+import DB.ConnectionDataBase;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(name = "AtlantisAddTypeOfRoomFirstSlotServlet", value = "/AtlantisAddTypeOfRoomFirstSlotServlet")
+public class AtlantisAddTypeOfRoomFirstSlotServlet extends HttpServlet {
+
+    Connection connection = ConnectionDataBase.connectToDataBase();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String typeOfRoom = request.getParameter("AddTypeOfRoom");
+
+        try{
+            insertTypeOfRoomIntoDatabase(typeOfRoom);
+            response.sendRedirect("../ManagerPages/AddTypeOfRoomSuccessFirstSlot.jsp");
+        }catch(SQLException e)
+        {
+            request.setAttribute("errorMessage","error updating database");
+            request.getRequestDispatcher("../ManagerPages/ErrorUpdatingDatabase.jsp").forward(request,response);
+        }
+    }
+    //    the functionality is called "try-with-resources" and is used for resources that implement the AutoCloseable or Closeable interfaces.
+    private void insertTypeOfRoomIntoDatabase(String typeOfRoom) throws SQLException {
+        String insertQuery = "UPDATE hoteli SET TypeOfRoom = ? WHERE ID = 1";
+        try (PreparedStatement insertStatement = connection.prepareStatement(insertQuery)) {
+            insertStatement.setString(1, typeOfRoom);
+            insertStatement.executeUpdate();
+        }
+    }
+}
