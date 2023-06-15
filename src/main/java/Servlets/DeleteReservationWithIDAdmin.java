30bac27f50c7e6a3cc98c68bf72ee7d5af28ed37
Goxy1 <uros.gojak001@gmail.com>
2023-06-15 13:08:55 +0200

    Created DeleteReservationWithIDAdmin
    Admin is able to delete the reservation just by entering the reservation ID

---
+++ src/main/java/Servlets/DeleteReservationWithIDAdmin.java
@@ -0,0 +1,63 @@
+package Servlets;
+
+import DB.ConnectionDataBase;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@WebServlet(name = "DeleteReservationWithIDAdmin", value = "/DeleteReservationWithIDAdmin")
+public class DeleteReservationWithIDAdmin extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Getting id from input type
+        String reservationID = request.getParameter("IDreservation");
+
+        // Connection with database
+        Connection connection = null;
+
+        try {
+            connection = ConnectionDataBase.connectToDataBase();
+
+            // Sql query that will delete reservation with their ID
+            String query = "DELETE FROM rezervacije WHERE ID = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, reservationID);
+
+            // Executing query
+            int rowsAffected = statement.executeUpdate();
+            statement.close();
+
+            // Checking if the reservation is successfully deleted
+            if (rowsAffected > 0) {
+                // Redirekcija na stranicu sa porukom o uspehu
+                response.sendRedirect("../AdminPages/SuccessfullyDeletedReservation.jsp");
+            } else {
+                // Redirekcija na stranicu sa porukom o grešci
+                response.sendRedirect("../ManagerPages/ErrorUpdatingDatabase.jsp");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Redirekcija na stranicu sa porukom o grešci
+            response.sendRedirect("errorPage.jsp");
+        } finally {
+            // Zatvaranje konekcije
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
