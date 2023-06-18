538eba047142ae87577fabafc01d882a6b9a0284
Goxy1 <uros.gojak001@gmail.com>
2023-06-18 12:51:36 +0200

    Added hotelPictures java class

---
+++ src/main/java/DB/HotelPictures.java
@@ -0,0 +1,62 @@
+package DB;
+
+public class HotelPictures {
+
+    private String outsidePicture;
+    private String outsidePicture2;
+    private String insidePicture;
+    private String insidePicture2;
+    private String HotelRoom;
+    private String HotelRoom2;
+
+    public HotelPictures() {
+    }
+
+    public String getOutsidePicture() {
+        return outsidePicture;
+    }
+
+    public void setOutsidePicture(String outsidePicture) {
+        this.outsidePicture = outsidePicture;
+    }
+
+    public String getOutsidePicture2() {
+        return outsidePicture2;
+    }
+
+    public void setOutsidePicture2(String outsidePicture2) {
+        this.outsidePicture2 = outsidePicture2;
+    }
+
+    public String getInsidePicture() {
+        return insidePicture;
+    }
+
+    public void setInsidePicture(String insidePicture) {
+        this.insidePicture = insidePicture;
+    }
+
+    public String getInsidePicture2() {
+        return insidePicture2;
+    }
+
+    public void setInsidePicture2(String insidePicture2) {
+        this.insidePicture2 = insidePicture2;
+    }
+
+    public String getHotelRoom() {
+        return HotelRoom;
+    }
+
+    public void setHotelRoom(String hotelRoom) {
+        HotelRoom = hotelRoom;
+    }
+
+    public String getHotelRoom2() {
+        return HotelRoom2;
+    }
+
+    public void setHotelRoom2(String hotelRoom2) {
+        HotelRoom2 = hotelRoom2;
+    }
+}
